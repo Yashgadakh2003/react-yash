takueof/next.js

Updates `draftMode().isEnabled` to be a Promise

test/e2e/app-dir/dynamic-io/app/draftmode/sync/page.tsx
@@ -1,0 +1,30 @@
+import { draftMode, type DangerouslyUnwrapDraftMode } from 'next/headers'
+
+import { getSentinelValue } from '../../getSentinelValue'
+
+import ToggleButton from './ToggleButton'
+
+export default async function Page() {
+  return (
+    <>
+      <p>
+        This page uses `draftMode().isEnabled`. This is now a promise however
+        reading it during prerender is fine becuase it is always false during
+        prerender so we don't expect it to trigger any dynamic behavior.
+      </p>
+      <Component />
+      <ToggleButton />
+      <div id="page">{getSentinelValue()}</div>
+    </>
+  )
+}
+
+async function Component() {
+  const syncDraftMode = draftMode() as unknown as DangerouslyUnwrapDraftMode
+  return (
+    <div>
+      draftMode enabled?{' '}
+      <span id="draft-mode">{'' + syncDraftMode.isEnabled}</span>
+    </div>
+  )
+}

test/e2e/app-dir/dynamic-io/app/draftmode/sync/toggle/route.ts
@@ -1,0 +1,13 @@
+import { draftMode, type DangerouslyUnwrapDraftMode } from 'next/headers'
+
+export async function GET(request: Request) {
+  const syncDraftMode = draftMode() as unknown as DangerouslyUnwrapDraftMode
+  const isEnabled = syncDraftMode.isEnabled
+  if (isEnabled) {
+    syncDraftMode.disable()
+    return new Response('Draft mode is disabled')
+  } else {
+    syncDraftMode.enable()
+    return new Response('Draft mode is enabled')
+  }
+}

Yashgadakh2003/react-yash

test/e2e/app-dir/dynamic-io/app/draftmode/sync/page.tsx
@@ -1,0 +1,30 @@
+import { draftMode, type UnsafeUnwrappedDraftMode } from 'next/headers'
+
+import { getSentinelValue } from '../../getSentinelValue'
+
+import ToggleButton from './ToggleButton'
+
+export default async function Page() {
+  return (
+    <>
+      <p>
+        This page uses `draftMode().isEnabled`. This is now a promise however
+        reading it during prerender is fine becuase it is always false during
+        prerender so we don't expect it to trigger any dynamic behavior.
+      </p>
+      <Component />
+      <ToggleButton />
+      <div id="page">{getSentinelValue()}</div>
+    </>
+  )
+}
+
+async function Component() {
+  const syncDraftMode = draftMode() as unknown as UnsafeUnwrappedDraftMode
+  return (
+    <div>
+      draftMode enabled?{' '}
+      <span id="draft-mode">{'' + syncDraftMode.isEnabled}</span>
+    </div>
+  )
+}

test/e2e/app-dir/dynamic-io/app/draftmode/sync/toggle/route.ts
@@ -1,0 +1,13 @@
+import { draftMode, type UnsafeUnwrappedDraftMode } from 'next/headers'
+
+export async function GET(request: Request) {
+  const syncDraftMode = draftMode() as unknown as UnsafeUnwrappedDraftMode
+  const isEnabled = syncDraftMode.isEnabled
+  if (isEnabled) {
+    syncDraftMode.disable()
+    return new Response('Draft mode is disabled')
+  } else {
+    syncDraftMode.enable()
+    return new Response('Draft mode is enabled')
+  }
+}