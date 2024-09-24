takueof/next.js

Updates tests to use async cookies

test/production/app-dir/symbolic-file-links/somewhere-else/src/i18n.ts
@@ -1,9 +1,11 @@
-import { cookies } from 'next/headers'
+import { cookies, type DangerouslyUnwrapCookies } from 'next/headers'
 
 // The purpose of this file is to demonstrate that without proper symbolic file checking
 // next accidentally marks files in the root of the project as client files.
 export default function () {
-  const locale = cookies().get('locale')?.value ?? 'en'
+  const locale =
+    (cookies() as unknown as DangerouslyUnwrapCookies).get('locale')?.value ??
+    'en'
 
   return {
     locale,

Yashgadakh2003/react-yash

test/production/app-dir/symbolic-file-links/somewhere-else/src/i18n.ts
@@ -1,9 +1,11 @@
-import { cookies } from 'next/headers'
+import { cookies, type UnsafeUnwrappedCookies } from 'next/headers'
 
 // The purpose of this file is to demonstrate that without proper symbolic file checking
 // next accidentally marks files in the root of the project as client files.
 export default function () {
-  const locale = cookies().get('locale')?.value ?? 'en'
+  const locale =
+    (cookies() as unknown as UnsafeUnwrappedCookies).get('locale')?.value ??
+    'en'
 
   return {
     locale,