takueof/next.js

`headers()` now returns an exotic promise of Headers
Similar to `cookies()`, `headers()` provides access to an underlying Request in Next.js. To be optimally compatible with dynamicIO we need request access to be implemented through async APIs and have udpated this function to return a promise of Headers rather than Headers directly. To facilitate migration this API will augment the Promise with properties that allow direct access to headers. In a future version of Next.js we will remove the exotic nature of this Promise and all access will require awaiting the result.

test/e2e/app-dir/dynamic-io/app/headers/exercise/sync/page.tsx
@@ -1,0 +1,32 @@
+import { headers, type DangerouslyUnwrapHeaders } from 'next/headers'
+
+import { getSentinelValue } from '../../../getSentinelValue'
+import { AllComponents } from '../commponents'
+
+export default async function Page() {
+  const xSentinelValues = new Set<string>()
+  // We use the async form here to avoid triggering dev warnings. this is not direclty being
+  // aserted, it just helps us do assertions in our AllComponents
+  for (let [headerName, headerValue] of (await headers()).entries()) {
+    if (headerName.startsWith('x-sentinel')) {
+      xSentinelValues.add(headerValue)
+    }
+  }
+
+  const allHeaders = headers() as unknown as DangerouslyUnwrapHeaders
+  return (
+    <>
+      <p>
+        This page will exercise a number of APIs on the headers() instance
+        directly (without awaiting it as a promise). It should not produce
+        runtime errors but it will warn in dev
+      </p>
+      <AllComponents
+        headers={allHeaders}
+        xSentinelValues={xSentinelValues}
+        expression="headers()"
+      />
+      <div id="page">{getSentinelValue()}</div>
+    </>
+  )
+}

test/e2e/app-dir/dynamic-io/app/headers/static-behavior/sync_boundary/page.tsx
@@ -1,0 +1,39 @@
+import { Suspense } from 'react'
+import { headers, type DangerouslyUnwrapHeaders } from 'next/headers'
+
+import { getSentinelValue } from '../../../getSentinelValue'
+/**
+ * This test case is constructed to demonstrate the deopting behavior of synchronously
+ * accesing dynamic data like headers. <ComponentTwo /> won't be able to render before we abort
+ * to it will bubble up to the root and mark the whoe page as dynamic when PPR is one. There
+ * is no real change in behavior when PPR is off.
+ */
+export default async function Page() {
+  return (
+    <>
+      <Suspense fallback="loading...">
+        <Component />
+      </Suspense>
+      <ComponentTwo />
+      <div id="page">{getSentinelValue()}</div>
+    </>
+  )
+}
+
+function Component() {
+  const _headers = headers() as unknown as DangerouslyUnwrapHeaders
+  const hasHeader = _headers.has('x-sentinel')
+  if (hasHeader) {
+    return (
+      <div>
+        header <span id="x-sentinel">{_headers.get('x-sentinel')}</span>
+      </div>
+    )
+  } else {
+    return <div>no header found</div>
+  }
+}
+
+function ComponentTwo() {
+  return <p>footer</p>
+}

test/e2e/app-dir/dynamic-io/app/headers/static-behavior/sync_root/page.tsx
@@ -1,0 +1,26 @@
+import { headers, type DangerouslyUnwrapHeaders } from 'next/headers'
+
+import { getSentinelValue } from '../../../getSentinelValue'
+
+export default async function Page() {
+  return (
+    <>
+      <Component />
+      <div id="page">{getSentinelValue()}</div>
+    </>
+  )
+}
+
+function Component() {
+  const _headers = headers() as unknown as DangerouslyUnwrapHeaders
+  const hasHeader = _headers.has('x-sentinel')
+  if (hasHeader) {
+    return (
+      <div>
+        header <span id="x-sentinel">{_headers.get('x-sentinel')}</span>
+      </div>
+    )
+  } else {
+    return <div>no header found</div>
+  }
+}

Yashgadakh2003/react-yash

test/e2e/app-dir/dynamic-io/app/headers/exercise/sync/page.tsx
@@ -1,0 +1,32 @@
+import { headers, type UnsafeUnwrappedHeaders } from 'next/headers'
+
+import { getSentinelValue } from '../../../getSentinelValue'
+import { AllComponents } from '../commponents'
+
+export default async function Page() {
+  const xSentinelValues = new Set<string>()
+  // We use the async form here to avoid triggering dev warnings. this is not direclty being
+  // aserted, it just helps us do assertions in our AllComponents
+  for (let [headerName, headerValue] of (await headers()).entries()) {
+    if (headerName.startsWith('x-sentinel')) {
+      xSentinelValues.add(headerValue)
+    }
+  }
+
+  const allHeaders = headers() as unknown as UnsafeUnwrappedHeaders
+  return (
+    <>
+      <p>
+        This page will exercise a number of APIs on the headers() instance
+        directly (without awaiting it as a promise). It should not produce
+        runtime errors but it will warn in dev
+      </p>
+      <AllComponents
+        headers={allHeaders}
+        xSentinelValues={xSentinelValues}
+        expression="headers()"
+      />
+      <div id="page">{getSentinelValue()}</div>
+    </>
+  )
+}

test/e2e/app-dir/dynamic-io/app/headers/static-behavior/sync_boundary/page.tsx
@@ -1,0 +1,39 @@
+import { Suspense } from 'react'
+import { headers, type UnsafeUnwrappedHeaders } from 'next/headers'
+
+import { getSentinelValue } from '../../../getSentinelValue'
+/**
+ * This test case is constructed to demonstrate the deopting behavior of synchronously
+ * accesing dynamic data like headers. <ComponentTwo /> won't be able to render before we abort
+ * to it will bubble up to the root and mark the whoe page as dynamic when PPR is one. There
+ * is no real change in behavior when PPR is off.
+ */
+export default async function Page() {
+  return (
+    <>
+      <Suspense fallback="loading...">
+        <Component />
+      </Suspense>
+      <ComponentTwo />
+      <div id="page">{getSentinelValue()}</div>
+    </>
+  )
+}
+
+function Component() {
+  const _headers = headers() as unknown as UnsafeUnwrappedHeaders
+  const hasHeader = _headers.has('x-sentinel')
+  if (hasHeader) {
+    return (
+      <div>
+        header <span id="x-sentinel">{_headers.get('x-sentinel')}</span>
+      </div>
+    )
+  } else {
+    return <div>no header found</div>
+  }
+}
+
+function ComponentTwo() {
+  return <p>footer</p>
+}

test/e2e/app-dir/dynamic-io/app/headers/static-behavior/sync_root/page.tsx
@@ -1,0 +1,26 @@
+import { headers, type UnsafeUnwrappedHeaders } from 'next/headers'
+
+import { getSentinelValue } from '../../../getSentinelValue'
+
+export default async function Page() {
+  return (
+    <>
+      <Component />
+      <div id="page">{getSentinelValue()}</div>
+    </>
+  )
+}
+
+function Component() {
+  const _headers = headers() as unknown as UnsafeUnwrappedHeaders
+  const hasHeader = _headers.has('x-sentinel')
+  if (hasHeader) {
+    return (
+      <div>
+        header <span id="x-sentinel">{_headers.get('x-sentinel')}</span>
+      </div>
+    )
+  } else {
+    return <div>no header found</div>
+  }
+}